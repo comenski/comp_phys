105e4004501e3d85ffb0fdf2ae7f0466dd2bbc32
comenski <comenius@posteo.net>
2016-07-12 13:18:00 +0200

    some progress, I hope

--- 5_verlet.py
+++ 5_verlet.py
@@ -3,36 +3,51 @@
 # import packages
 import numpy as np
 import verlet
-from scipy.constants import g
+from scipy.constants import g as GRAV, k as kB
+import pylab
 
 # declare constants, variables etc.
 SIGMA=3.405e-10
 EPSILON=1.651e-21
 MASS=6.644e-26
 numParticles = 100
+boxLength = 5 # TODO get box length from volume taken by 100*10 particles as given by ideal gas theory OR 10*particle free range, whichever is bigger
+TEMP = 288.15
+VAR = (kB*TEMP/MASS)**(1/2)
 
 initCoords = []
+xList = np.random.sample(numParticles)
+yList = np.random.sample(numParticles)
 for i in range(numParticles):
-    xList = np.random.sample(100)
-    yList = np.random.sample(100)
-    initCoords.append([xList[i],yList[i]]*length)
+    initCoords.append([xList[i],yList[i]]*boxLength)
 
-initVel0 = create [[dxi,dyi]*numParticles] # create array according to maxwell-boltzmann
+# create velocity, x and y normal distributed -> [x,y] maxwell-boltzmann distributed
+# sum over all velocities to get net momentum of all particles, substract net momentum/number of particles from each velocity to retain distribution and get zero net momentum
 
-AvgVel = [0,0]
+initVel0 = []
+dxList = np.random.normal(0,VAR,100)
+dyList = np.random.normal(0,VAR,100)
+for i in range(numParticles):
+    initVel0.append([dxList[i],dyList[i]])
+
+netTotalVel = [0,0]
 for i in range(len(initVel0)):
-    AvgVel += initVel[i]
+    for j in range(len(initVel0[i])):
+        netTotalVel[j] += initVel0[i][j]
 
 initVel = initVel0
 for i in range(len(initVel0)):
-    initVel[i] -= AvgVel/len(initVel0)
+    for j in range(len(netTotalVel)):
+        initVel[i][j] -= netTotalVel[j]/len(initVel0)
 
-#define interaction according to leonard-jones-potential
+#-------------------------------
+
+#define interaction according to leonard-jones-potential for problems 5,7
 def forceLJ(r):
     R=SIGMA/r
     return 4*EPSILON*(12*R**13-6*R**7)*[xj-xi,yj-yi]
 
-FG = [0,g*MASS]
+FG = [0,GRAV*MASS]
 def forceLJG(r):
     R=SIGMA/r
     return 4*EPSILON*(12*R**13-6*R**7)*[xj-xi,yj-yi]+FG
