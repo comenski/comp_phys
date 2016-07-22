f46aa8cd099034ff7ffc52b041792dbec652b3ac
comenski <comenius@posteo.net>
2016-07-22 22:36:33 +0200

    stupid matrix... on the plus side: not one loop used!

--- initialization.py
+++ initialization.py
@@ -5,11 +5,11 @@ from scipy.constants import g as GRAV, k as kB
 SIGMA=3.405e-10
 EPSILON=1.651e-21
 MASS=6.644e-26
-numParticles = 100 
+numParticles = 100
 boxLength = 5 # TODO get box length from volume taken by 100*10 particles as given by ideal gas theory OR 10*particle free range, whichever is bigger
 TEMP = 288.15
 VAR = (kB*TEMP/MASS)**(1/2) # According to ideal gas theory
-rMax = 0.5*boxLength
+rMax = 0.5*boxLength #TODO completely arbitrary atm
 
 
 # get initial coordinates, randomly distributed in space # TODO write to data file[0]
@@ -34,27 +34,37 @@ for i in range(len(initVel0)):
     for j in range(len(netTotalVel)):
         initVel[i][j] -= netTotalVel[j]/len(initVel0)
 
-#------------------------------- 
+#-------------------------------
 
-# define interaction according to leonard-jones-potential for problems 5,7 
-# goes into respective module to be used by verlet.py or 
-# goes directly into verlet.py or gets provided from definitions.py to verlet.py and module takes an additional argument LJ/LJG/throw for force 
- 
-FG = [0,GRAV*MASS] 
- 
- 
-#def forceLJ(r):
-#   return 4*EPSILON*(12*SIGMA**12/r**14 - 6*SIGMA**6/r**8)*[xj-xi,yj-yi] #TODO correct nomenclature
- 
+# define interaction according to leonard-jones-potential for problems 5,7
+# goes into respective module to be used by verlet.py or
+# goes directly into verlet.py or gets provided from definitions.py to verlet.py and module takes an additional argument LJ/LJG/throw for force
+
+FG = [0,GRAV*MASS]
+
 # let's try with vector this time:
 rMatrix = np.tile(initCoords,(numParticles,1)).reshape(numParticles,numParticles,2)
 rMatrix -= np.transpose(rMatrix,(1,0,2))
 rMatrix = np.ma.masked_outside(rMatrix, -rMax, rMax)
+rMatrix = np.ma.filled(rMatrix, 0)
+
+rMatrixBound1 = np.tile(initCoords,(numParticles,1)).reshape(numParticles,numParticles,2)
+rMatrixBound1 -= np.transpose(rMatrixBound1+[0,boxLength],(1,0,2))
+rMatrixBound1 = np.ma.masked_outside(rMatrixBound1, -rMax, rMax)
+rMatrixBound1 = np.ma.filled(rMatrixBound1, 0)
+
+rMatrixBound2 = np.tile(initCoords,(numParticles,1)).reshape(numParticles,numParticles,2)
+rMatrixBound2 -= np.transpose(rMatrixBound2+[boxLength,0],(1,0,2))
+rMatrixBound2 = np.ma.masked_outside(rMatrixBound2, -rMax, rMax)
+rMatrixBound2 = np.ma.filled(rMatrixBound2, 0)
+
+rMatrix += rMatrixBound1 + rMatrixBound2
 rMatrix = np.ma.masked_values(rMatrix, 0)
+
 print(rMatrix)
 
 
-def forceLJ(r,i,j):
+def forceLJ(r):
     [xi,yi,xj,yj]=[r[i][0],r[i][1],r[j][0],r[j][1]]
     rAbs = math.sqrt(xi**2-xj**2+yi**2-yj**2)
     if rAbs < rMax:
@@ -73,13 +83,13 @@ def forceLJ(r,i,j):
 
 
 #----------------------------------------------OLD STUF, PROBS NO LONGER APPLICABLE
- 
-# TODO two better ideas performance-wise for forceLJG: 
-# use forceLJ(r) for heated case as well, add after summation over all j has happened (doesn't need verlet b/c FG is constant): 
-# -------- goes to the end of iteration(n) ------------------ 
-# drFG = integral(1/2*GRAV*dt**2) 
-# dvFG = GRAV*dt 
-# if j==numParticles+1: 
-#     rho+=drFG , nu+=dvFG , n 
-# this is equal to treating gravity as n+1th particle with constant interaction and should be warranted, given the 
+
+# TODO two better ideas performance-wise for forceLJG:
+# use forceLJ(r) for heated case as well, add after summation over all j has happened (doesn't need verlet b/c FG is constant):
+# -------- goes to the end of iteration(n) ------------------
+# drFG = integral(1/2*GRAV*dt**2)
+# dvFG = GRAV*dt
+# if j==numParticles+1:
+#     rho+=drFG , nu+=dvFG , n
+# this is equal to treating gravity as n+1th particle with constant interaction and should be warranted, given the
 # small timesteps used for simulation and the relative weakness of gravitational pull
